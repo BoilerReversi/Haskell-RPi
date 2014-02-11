9d2e518703f925cd93e581dd1a1a45990bc12fee
ramirez7 <ramirez7@purdue.edu>
2014-02-11 21:15:25 +0000

    updated framework and started README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Haskell
+This repo is just going to be a mix of code that we use on the RPi. It will eventually be sectioned out into individual repos/modules
+## TODO
+* Put Haskell RPi framework in module/its own repo
+* Create more InputActions and OutputActions. For instance, for GPIO output or UART input
--- cc.hs
+++ cc.hs
@@ -4,12 +4,12 @@ import Control.Concurrent
 import Control.Monad
 import Control.Concurrent.Chan
 
-main = let inputs = [newGpioEvent 7 g]
-           outputs = [Immediate print]
-           update msg state = msg
-       in  topLevel inputs update Reset outputs
+main = let inputs = [newPeriodicEvent 1000000 True]
+           outputs = [Immediate (print . head)]
+           update msg state = tail state
+       in  topLevel inputs update (cycle "GO PURDUE ") outputs
 
-data Message = Undo | Reset | Legal Bool deriving(Show)
+data Message = Undo | Reset | Legal Bool deriving (Show)
 
 f Rising = Just Undo
 f Falling = Nothing
@@ -18,9 +18,25 @@ g Rising = Just (Legal True)
 g Falling = Just (Legal False)
 
 -- BEGIN: OUR FRAMEWORK
-data OutputAction s = Immediate (s -> IO ()) | Repeat Int (s -> IO ())
+-- Will eventually be in some module like
+-- RPi.RaspberryLambda.something. Naming is difficult actually
 
-topLevel :: [(Chan m -> IO ())] ->
+-- I don't like this type synonym actually.
+type InputAction m = (m -> IO ()) -> IO ()
+
+data OutputAction s = Immediate (s -> IO ()) 
+                    | Repeat Int (s -> IO ())
+
+-- Rename this to something less dumb than topLevel
+-- This function will...
+-- Tie each InputAction to the same central Chan, to which each
+--  InputAction will push messages of type m
+-- Fork on each InputAction to listen to each event concurrently
+-- Pull messages out of the central Chan and update state according to
+--  (m -> s -> s)
+-- Push updated state to concurrent output handlers, which will produce
+-- output according to the provided OutputAction
+topLevel :: [InputAction m] ->
             (m -> s -> s) ->
             s ->
             [OutputAction s] ->
@@ -29,7 +45,7 @@ topLevel inputs update initial outputs =
   do
     -- fork and listen to inputs
     ch <- newChan
-    mapM_ forkIO $ map ($ ch) inputs
+    mapM_ forkIO $ map ($ writeChan ch) inputs
     -- fork and wait on outputs
     -- outStates is a list of MVar s
     outStates <- mapM newMVar $ replicate (length outputs) initial
@@ -42,10 +58,17 @@ topLevel inputs update initial outputs =
                                  loop ch outStates newState
 
 
+-- Changes the MVar's contents to the inputted value, whether or
+-- not the MVar is full. Only works as expected if the MVar has
+-- ONE and only ONE producer
 updateMVar :: MVar a -> a -> IO ()
 updateMVar mv val = do tryTakeMVar mv
                        putMVar mv val
 
+-- Creates an OutputHandler IO () (to be forked presumably) that will
+-- read state from MVar and output according to it. Immediate OutputActions
+-- will occur as soon as state is updated*. Repeat OutputActions will
+-- occur at a set interval regardless of updated state
 newOutputHandler :: OutputAction s -> MVar s -> IO ()
 newOutputHandler (Immediate output) mv = forever $ do state <- takeMVar mv
                                                       output state
@@ -61,18 +84,25 @@ newOutputHandler (Repeat interval output) mv =
                     threadDelay interval
                     loop nextState
 
--- Helpers to plug into top-level
-data Edge = Rising | Falling deriving(Show)
-
-newGpioEvent :: Int -> (Edge -> Maybe m) -> Chan m -> IO ()
-newGpioEvent pinNumber newMsg ch = do p <- P.init pinNumber In
-                                      loop p Zero
+-- Gpio listener wrapper. Will push the unwrapped result of 
+-- Edge -> Maybe m if it returns Just. So you can use that function
+-- to choose to fire messages on the rising and/or falling edges
+-- of GPIO.
+data Edge = Rising | Falling deriving (Show)
+newGpioEvent :: Int -> (Edge -> Maybe m) -> InputAction m
+newGpioEvent pinNumber newMsg action = do p <- P.init pinNumber In
+                                          loop p Zero
   where
     loop pin prev = do
       curr <- P.read pin
       let reaction = case (prev, curr) of (Zero, One) -> newMsg Rising
                                           (One, Zero) -> newMsg Falling
                                           (_, _) -> Nothing
-      case reaction of (Just msg) -> writeChan ch msg
+      case reaction of (Just msg) -> action msg
                        Nothing -> return ()
       loop pin curr
+
+-- Periodic event. Will periodically input m to the Chan
+newPeriodicEvent :: Int -> m -> InputAction m
+newPeriodicEvent interval msg action = forever $ do action msg
+                                                    threadDelay interval
