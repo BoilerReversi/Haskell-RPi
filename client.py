6bea056140b962607be89051063e07d006297b8c
ramirez7 <ramirez7@purdue.edu>
2014-05-01 20:23:14 +0000

    save/load added. asks for server ip on startup

--- client.py
+++ client.py
@@ -1,14 +1,14 @@
 import socket
 import sys
 
-HOST = '10.184.35.63'
+HOST = '10.184.47.67'
 PORT = 5678
 
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
+    s.connect((sys.argv[1], PORT))
 #    s.send("-------------------X-------XX------XO--------------------------- O"+"\n")
-    s.send(sys.argv[1]+"\n\n")
+    s.send(sys.argv[2]+"\n\n")
     
     data = s.recv(2048)
     data1 = s.recv(2048)
--- othello.hs
+++ othello.hs
@@ -6,18 +6,17 @@ import qualified Data.ByteString as B
 import Board
 import Data.Word
 import System.Process(readProcess)
+import System.Directory (doesFileExist)
+savePath :: String
+savePath = "OTHELLOSAVE"
 
 data GameState = GameState { board :: [Board]
                            , ai :: Bool
-                           , legal :: Bool } deriving (Show)
+                           , legal :: Bool 
+                           , save :: Bool} deriving (Show)
 
 data Message = Undo | Reset | Mark (Int, Int)
-             | Save | Load | Suggest Bool | Legal Bool deriving (Show)
-
--- TODO
--- Get inputs working (uart, buttons (legal, undo, ai, reset))
--- State just falls through
--- print most recent message
+             | Save | Load Board | Suggest Bool | Legal Bool deriving (Show)
 
 byteToCoord :: Int -> (Int, Int)
 byteToCoord w = (w `div` 8, w `mod` 8)
@@ -28,7 +27,21 @@ uartListener s = do r <- recv s 1
                       [] -> uartListener s
                       (w:_) 
                         | w < 64 -> return $ Mark $ byteToCoord $ fromIntegral w
+                        | w == 75 -> do e <- doesFileExist savePath
+                                        if e
+                                          then do print "Loading `OTHELLOSAVE`"
+                                                  x <- readFile savePath
+                                                  print $ "LOADED " ++ x
+                                                  return $ Load $ fromEdaxString x
+                                          else uartListener s
+
+                        | w == 95 -> do print "got 95!"
+                                        return Save
                         | otherwise -> uartListener s
+
+saveAction :: GameState -> IO ()
+saveAction (GameState (b:_) _ _ True) = writeFile savePath $ toEdaxString b
+saveAction (GameState _ _ _ False) = return ()
 
 risingListener :: P.Pin -> m -> IO m
 risingListener n msg = do e <- getGpioPinEdge n
@@ -53,17 +66,17 @@ uartBoard s b = mapM_ (mysend s) $ boardToWords squareToColor b
 
 mysend s w = send s $ B.pack [w]
 
-outputGameState :: SerialPort -> GameState -> IO ()
-outputGameState s (GameState b False False) = uartBoard s $ outputList False False $ head b
-outputGameState s g@(GameState b _ True) = uartBoard s $ outputList True False $ head b
-outputGameState s g@(GameState b True False) = do print "DOING AI"
-                                                  x <- readProcess "python" ["client.py", toEdaxString (head b)] []
-                                                  let c = j x
-                                                      ls = legalMoves (head b)
-                                                  if (isLegal (head b) c)
-                                                    then uartBoard s $ replaceAt (h x) (outputList False False $ head b) AI
-                                                    else uartBoard s $ outputList False True $ (head b)
-                                                  print x
+outputGameState :: SerialPort -> String -> GameState -> IO ()
+outputGameState s _ (GameState b False False _) = uartBoard s $ outputList False False $ head b
+outputGameState s _ g@(GameState b _ True _) = uartBoard s $ outputList True False $ head b
+outputGameState s ip g@(GameState b True False _) = do print "DOING AI"
+                                                       x <- readProcess "python" ["client.py", ip, toEdaxString (head b)] []
+                                                       let c = j x
+                                                           ls = legalMoves (head b)
+                                                       if (isLegal (head b) c)
+                                                         then uartBoard s $ replaceAt (h x) (outputList False False $ head b) AI
+                                                         else uartBoard s $ outputList False True $ (head b)
+                                                       print x
                          
   where
     f 'A' = 0
@@ -90,24 +103,29 @@ outputGameState s g@(GameState b True False) = do print "DOING AI"
     j (x:y:_) = (f x, g y)
     k (x, y) = (x * 8) + y
     replaceAt n xs v = take n xs ++ [v] ++ drop (n + 1) xs
+
 doAI :: GameState -> IO ()
-doAI g@(GameState b False _) = return ()
-doAI g@(GameState b True _) = do print "DOING AI"
-                                 x <- readProcess "python" ["client.py", toEdaxString (head b)] []
-                                 print x
+doAI g@(GameState b False _ _) = return ()
+doAI g@(GameState b True _ _) = do print "DOING AI"
+                                   x <- readProcess "python" ["/root/Haskell-RPi/client.py", toEdaxString (head b)] []
+                                   print x
 
 updateGameState :: Message -> GameState -> GameState
 updateGameState (Mark c) g = if (isLegal (head $ board g) c)
-                             then g { board = ((makeMove (head $ board g) c) : (board g)) }
-                             else g
+                             then g { board = ((makeMove (head $ board g) c) : (board g)), save = False }
+                             else g { save = False }
 updateGameState Undo g = case (board g) of
-  [x] -> g
-  (x:xs) -> g {board = xs}
-updateGameState Reset g = GameState {board = [initialBoard], ai = False, legal = False }
-updateGameState (Suggest x) g = g { ai = x }
-updateGameState (Legal x) g = g { legal = x }
+  [x] -> g {save = False}
+  (x:xs) -> g {board = xs, save = False}
+updateGameState Reset g = GameState {board = [initialBoard], ai = False, legal = False , save = False}
+updateGameState (Suggest x) g = g { ai = x , save = False}
+updateGameState (Legal x) g = g { legal = x , save = False}
+updateGameState Save g = g {save = True}
+updateGameState (Load b) g = g {board = [b], save = False}
 
-main = do undoPin <- P.init 0 P.In
+main = do putStrLn "Enter server IP:"
+          ip <- getLine
+          undoPin <- P.init 0 P.In
           resetPin <- P.init 1 P.In
           aiPin <- P.init 7 P.In
           legalPin <- P.init 3 P.In
@@ -117,8 +135,9 @@ main = do undoPin <- P.init 0 P.In
                                                     risingListener resetPin Reset,
                                                     edgeListener aiPin Suggest,
                                                     edgeListener legalPin Legal]
-              initial = GameState {board = [initialBoard], ai = False, legal = False }
+              initial = GameState {board = [initialBoard], ai = False, legal = False, save = False }
               update = updateGameState
               outputs = [OutputAction Immediate $ (printBoard . head . board),
-                         OutputAction Immediate $ outputGameState s]
+                         OutputAction Immediate $ outputGameState s ip,
+                         OutputAction Immediate $ saveAction]
           topLevel inputs update initial outputs
